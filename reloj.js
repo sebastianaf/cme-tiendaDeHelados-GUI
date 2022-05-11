4fa6a5b4f390a855ec373105b96a62072d7e410e
sebastianaf <sebastianafanador95@gmail.com>
2022-05-11 07:59:12 -0500

    v3.0.1
    FIX removing white spaces

--- reloj.js
+++ reloj.js
@@ -2,6 +2,7 @@ const reloj = () => {
   let miReloj = document.getElementById("reloj");
 
   let fechaActual = new Date();
+  
   let horas = fechaActual.getHours();
   let minutos = fechaActual.getMinutes();
   let segundos = fechaActual.getSeconds();
@@ -9,15 +10,7 @@ const reloj = () => {
 
   setInterval(() => {
     let horaActual = `
-    
-    ${horas < 10 ? "0" : ""}
-    ${horas + parseInt(parseInt(segundos / 60) / 60)}:
-    
-    ${minutos % 60 < 10 ? "0" : ""}
-    ${minutos + parseInt(segundos / 60)}:
-    
-    ${segundos % 60 < 10 ? "0" : ""}
-    ${segundos % 60}`;
+    ${horas < 10 ? "0" : ""}${horas + parseInt(parseInt(segundos / 60) / 60)}:${minutos % 60 < 10 ? "0" : ""}${minutos + parseInt(segundos / 60)}:${segundos % 60 < 10 ? "0" : ""}${segundos % 60}`.replace(/ /g, "");
     miReloj.innerHTML = horaActual;
     segundos++;
   }, 1000);
